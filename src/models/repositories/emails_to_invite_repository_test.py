2e39dc00a07f97cf7f7ace316386180b318416cc
Nuno Melo <67382746+nunomelo98@users.noreply.github.com>
2024-07-11 17:14:18 -0100

    feat implementing links

--- src/models/repositories/emails_to_invite_repository_test.py
+++ src/models/repositories/emails_to_invite_repository_test.py
@@ -4,7 +4,7 @@ from src.models.settings.db_connection_handler import db_connection_handler
 from .emails_to_invite_repository import EmailsToInviteRepository
 
 db_connection_handler.connect()
-trip_id = 11
+trip_id = str(uuid.uuid4())
 
 @pytest.mark.skip(reason="interacao com a base de dados")
 def test_resgistry_email():
